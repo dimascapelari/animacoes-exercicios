bb5ba0d669b426b620e3db7d6984015eb7d66116
Dimas Capelari <dimas.capelari@gmail.com>
2022-08-31 09:08:19 -0300

    225 - animando componentes dinamicos

--- src/App.vue
+++ src/App.vue
@@ -49,11 +49,36 @@
     >
       <div v-if="exibir2" class="caixa"></div>
     </transition>
+
+    <hr />
+    <div class="mb-4">
+      <b-button
+        variant="primary"
+        class="mr-2"
+        @click="componenteSelecionado = 'AlertaInfo'"
+        >Info</b-button
+      >
+      <b-button
+        variant="secondary"
+        @click="componenteSelecionado = 'AlertaAdvertencia'"
+        >Advertência</b-button
+      >
+    </div>
+    <transition name="fade" mode="out-in">
+      <component :is="componenteSelecionado"></component>
+    </transition>
   </div>
 </template>
 
 <script>
+import AlertaAdvertencia from "./AlertaAdvertencia.vue";
+import AlertaInfo from "./AlertaInfo.vue";
+
 export default {
+  components: {
+    AlertaAdvertencia,
+    AlertaInfo,
+  },
   data() {
     return {
       msg: "Uma mensagem de informação para o usuário!",
@@ -61,6 +86,7 @@ export default {
       exibir2: true,
       tipoAnimacao: "fade",
       larguraBase: 0,
+      componenteSelecionado: "AlertaInfo",
     };
   },
   methods: {
